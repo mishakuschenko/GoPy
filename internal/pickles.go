d3b998838680ef39a55e047e85e52f6a19c956b6
mishakuschenko <liahimkusenko@gmail.com>
2024-11-04 00:12:52 +0300

    добавил окно регистрации и маршруты в run.go, пока что только над фронтом работаю...

--- internal/pickles.go
+++ internal/pickles.go
@@ -1,24 +1,31 @@
-package internal;
+package internal
 
 import (
 	"html/template"
-	"net/http"
 	"log"
-);
+	"net/http"
+)
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	type TitleOfPage struct {
-		Title string
-	};
-	Index := TitleOfPage{
-		Title: "GoPy!",
-	};
-	temp, err := template.ParseFiles("../templates/index.html");
+	temp, err := template.ParseFiles("../templates/index.html")
 	if err != nil {
-		log.Fatal("File not found");
-	};
-	
-	temp.Execute(w, Index);
-};
+		log.Fatal("File not found")
+	}
+	temp.Execute(w, nil)
+}
 
+func Login(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("../templates/login.html")
+	if err != nil {
+		log.Fatal("File not found")
+	}
+	temp.Execute(w, nil)
+}
 
+func Reg(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("../templates/reg.html")
+	if err != nil {
+		log.Fatal("File not found")
+	}
+	temp.Execute(w, nil)
+}
--- internal/run.go
+++ internal/run.go
@@ -1,25 +1,26 @@
-package internal;
+package internal
 
 import (
-	"github.com/go-chi/chi/v5"
-	"net/http"
-	"github.com/go-chi/chi/v5/middleware"
 	"flag"
-);
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
 
 func Run() {
-	initLogger := flag.Bool("run_logger", false, "Run logger - yes or no");
-	flag.Parse();
+	initLogger := flag.Bool("run_logger", false, "Run logger - yes or no")
+	flag.Parse()
 
-	r := chi.NewRouter();
+	r := chi.NewRouter()
 	if *initLogger == true {
-		r.Use(middleware.Logger);
-	};
-	
-	fileServer := http.FileServer(http.Dir("../static/"))
-  r.Handle("/static/*", http.StripPrefix("/static", fileServer))
-	
-	r.Get("/", Index);
+		r.Use(middleware.Logger)
+	}
 
-	http.ListenAndServe(":8080", r);
-};
+	fileServer := http.FileServer(http.Dir("../static/"))
+	r.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	r.Get("/", Index)
+	r.Get("/login/", Login)
+	r.Get("/reg/", Reg)
+	http.ListenAndServe(":8080", r)
+}
